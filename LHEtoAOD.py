81ede3f906d185c31cf21f00faaaada4b771565c
Martin <ka_hei_martin_kwok@brown.edu>
2016-07-01 14:25:55 +0200

    init

---
+++ LHEtoAOD.py
@@ -0,0 +1,177 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/Generator/python/Hadronizer_TuneCUETP8M1_13TeV_generic_LHE_pythia8_cff.py --filein file:BlackMaxLHArecord.lhe --datamix PreMix --conditions MCRUN2_74_V9 --pileup_input dbs:/Neutrino_E-10_gun/RunIISpring15PrePremix-MCRUN2_74_V9-v1/GEN-SIM-DIGI-RAW --fast -n -1 --eventcontent AODSIM -s GEN,SIM,RECOBEFMIX,DIGIPREMIX_S2,DATAMIX,L1,L1Reco,RECO,HLT:@relval25ns --datatier AODSIM --beamspot NominalCollision2015 --customise SLHCUpgradeSimulations/Configuration/postLS1CustomsPreMixing.customisePostLS1 --magField 38T_PostLS1 --fileout MLM_BlackMax.root
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('FastSimulation.Configuration.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('FastSimulation.Configuration.Geometries_MC_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedNominalCollision2015_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('FastSimulation.Configuration.SimIdeal_cff')
+process.load('FastSimulation.Configuration.Reconstruction_BefMix_cff')
+process.load('FastSimulation.Configuration.DigiDMPreMix_cff')
+process.load('SimGeneral.MixingModule.digi_MixPreMix_cfi')
+process.load('FastSimulation.Configuration.DataMixerPreMix_cff')
+process.load('FastSimulation.Configuration.SimL1Emulator_cff')
+process.load('FastSimulation.Configuration.L1Reco_cff')
+process.load('FastSimulation.Configuration.Reconstruction_AftMix_cff')
+process.load('HLTrigger.Configuration.HLT_25ns14e33_v1_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+#------------------------------------------------------------------------------------
+# Options
+#------------------------------------------------------------------------------------
+
+options = VarParsing.VarParsing()
+options.register('InputFile',
+		"file:input.lhe",
+		VarParsing.VarParsing.multiplicity.singleton,
+		VarParsing.VarParsing.varType.string,
+		"filename of LHE")
+
+
+options.parseArguments()
+
+fname = options.InputFile.split("/")
+OutputFile = fname[len(fname)-2]+"_AOD.root"
+print "Input LHE =", options.InputFile
+print "Onput AOD =", OutputFile
+
+#------------------------------------------------------------------------------------
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("LHESource",
+#   fileNames = cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/user/tutanon/BH2015/LHE/BlackMaxn4/BH2_BM_MD2000_MBH10000_n4/BlackMaxLHArecord.lhe') 
+#   fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/b/belotel/work/public/BH/BH6_CH_MD2000_MBH6000_n2/lhouches.xml') 
+    fileNames = cms.untracked.vstring(
+	 options.InputFile
+    )
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/Generator/python/Hadronizer_TuneCUETP8M1_13TeV_generic_LHE_pythia8_cff.py nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('AODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    #fileName = cms.untracked.string('TEST.root'),				       
+    fileName = cms.untracked.string(OutputFile),				       
+    outputCommands = process.AODSIMEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+process.mix.digitizers = cms.PSet(process.theDigitizersMixPreMix)
+process.mixData.input.fileNames = cms.untracked.vstring(['/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/0A977B86-9026-E511-8B64-0025905AA9CC.root', '/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/0C815C87-AE26-E511-BEB5-0025905A48C0.root', '/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/144FF8B9-8726-E511-AF47-0025905938D4.root', '/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/149937C9-8526-E511-A6B7-0025905B85EE.root','/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/1EB85210-8726-E511-A139-0025905A607E.root','/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/1EB85210-8726-E511-A139-0025905A607E.root','/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/28B6D74F-9226-E511-9B3E-0025905964C4.root','/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/2AF81D4F-A926-E511-BAFF-0025905A48FC.root', '/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/34DBF3EB-A326-E511-80F1-0025905A60B2.root','/store/mc/RunIISpring15PrePremix/Neutrino_E-10_gun/GEN-SIM-DIGI-RAW/MCRUN2_74_V9-v1/40001/36B97117-9326-E511-A9D3-002354EF3BDB.root'])
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_74_V9', '')
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.reconstruction_befmix_step = cms.Path(process.reconstruction_befmix)
+process.digitisation_step = cms.Path(process.pdigi)
+process.datamixing_step = cms.Path(process.pdatamix)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.reconstruction_befmix_step,process.digitisation_step,process.datamixing_step,process.L1simulation_step,process.L1Reco_step,process.reconstruction_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.endjob_step,process.AODSIMoutput_step])
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from FastSimulation.Configuration.MixingModule_Full2Fast
+from FastSimulation.Configuration.MixingModule_Full2Fast import prepareDigiRecoMixing 
+
+#call to customisation function prepareDigiRecoMixing imported from FastSimulation.Configuration.MixingModule_Full2Fast
+process = prepareDigiRecoMixing(process)
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1CustomsPreMixing
+from SLHCUpgradeSimulations.Configuration.postLS1CustomsPreMixing import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1CustomsPreMixing
+process = customisePostLS1(process)
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforFastSim 
+
+#call to customisation function customizeHLTforFastSim imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforFastSim(process)
+
+# End of customisation functions
+
